253ba202123206e40940c231be1df5bfad9c13b7
nicordo19 <poiraudnico@gmail.com>
2024-12-13 12:26:00 +0100

    first commit

---
+++ kata.js
@@ -0,0 +1,82 @@
+// const h1 = document.querySelector("#text");
+// let bouton1 = document.querySelector("#bouton1");
+// let bouton2 = document.querySelector("#bouton2");
+// let bouton3 = document.querySelector("#bouton3");
+// let bouton4 = document.querySelector("#bouton4");
+
+// // const bouton = [bouton1, bouton2, bouton3, bouton4];
+
+// bouton1.addEventListener("click", function couleurBleu() {
+//   bouton1.style.background = "blue";
+//   h1.innerHTML = " tu a trouver la bonne couleur";
+// });
+
+// bouton2.addEventListener("click", function couleurJaune() {
+//   bouton2.style.background = "yellow";
+//   h1.innerHTML = " continue a cherher";
+//   if (bouton2) {
+//     bouton2 = false;
+//     console.log(bouton2);
+//   }
+// });
+// bouton3.addEventListener("click", function couleurViolet() {
+//   bouton3.style.background = "purple";
+//   h1.innerHTML = " continue a cherher";
+//   if (bouton3) {
+//     bouton3 = false;
+//     console.log(bouton3);
+//   }
+// });
+// bouton4.addEventListener("click", function couleurRouge() {
+//   bouton4.style.background = "red";
+//   h1.innerHTML = " continue a cherher";
+//   if (bouton4) {
+//     bouton4 = false;
+//     console.log(bouton4);
+//   }
+// });
+
+// function trouverLaCouleur() {
+//   bouton1.addEventListener("click", function couleurBleu() {
+//     bouton1.style.background = "blue";
+//   });
+//   for (let i = 0; i < i.length; i++) {
+//     if (bouton1) {
+//       h1.innerHTML = " vous avez trouver la bonne couleur";
+//     } else {
+//       h1.innerHTML = " continue a chercher ";
+//     }
+//   }
+// }
+// trouverLaCouleur();
+// function clickSurLaBonneCouleur() {
+//   for (let i = 0; i < bouton.length; i++) {
+//     if (bouton.bouton1) {
+//     }
+//   }
+// }
+
+let couleur = ["blue", "yellow", "purple", "red"];
+console.log(couleur);
+let possible = 12;
+
+function verifiLesCouleur() {
+  for (let i = 0; i < couleur.length; i++) {
+    if (couleur) {
+      couleur[i] = true;
+      console.log(couleur[i]);
+    } else {
+      false;
+    }
+  }
+  function bonneReponce(reponce) {
+    let bonneCouleur = ["blue", "red"];
+    if (bonneCouleur === "blue" && "red") {
+      bonneCouleur = true;
+    } else {
+      bonneCouleur = fels;
+    }
+  }
+  return bonneCouleur;
+}
+verifiLesCouleur(bonneCouleur);
